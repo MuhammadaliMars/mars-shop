0dc7bf97346a25c0a04daf47d8b3aabdf4c6f415
MuhammadaliMars <marsmuhammadali@gmail.com>
2023-12-22 05:07:35 -0800

    asas

--- app.py
+++ app.py
@@ -1,27 +1,24 @@
-from aiogram import Dispatcher, Bot, types, executor
-from default_keyboards import user_main_menu
+from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
 
+from default_keyboards import user_main_menu
 from environs import Env
 
-env= Env()
+env = Env()
 env.read_env()
 
 BOT_TOKEN = env.str("TOKEN")
 
 storage = MemoryStorage()
 bot = Bot(token=BOT_TOKEN, proxy="http://proxy.server:3128")
-dp = Dispatcher(bot=bot, storage=storage)
+dp = Dispatcher(bot, storage=storage)
 
 
 @dp.message_handler(commands="start")
 async def start_handler(message: types.Message):
-    text = "assalomu aleykum"
+    text = "Botimizga xush kelibsiz."
     await message.answer(text=text, reply_markup=user_main_menu)
-    
-
-
-       
 
 
 if __name__ == "__main__":
